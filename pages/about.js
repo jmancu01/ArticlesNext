d74d0bf05be0ba350f64b16880900720bae74b40
jmancu01 <73651528+jmancu01@users.noreply.github.com>
2021-12-31 10:48:20 -0300

    first

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,8 @@ const about = () => {
   return (
     <div>
       <Meta title='About' />
-      <h1>About</h1>
+      <h1>Transformamos el cultivo sojero en un activo digital, una nueva forma de guardar valor y generar ganancias.</h1>
+      <h4>Sojacoin, es un criptoactivo respaldado en pool de siembra, cada token representa una parte de campo.</h4>
     </div>
   )
 }
